d9226386713f61c65b31d1db2adbc0c8ace93aad
Kevin Murray <kdmfoss@gmail.com>
2018-10-25 17:51:41 +1100

    Super basic setup.py

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+from glob import glob
+#import versioneer
+
+
+#versioneer.VCS = 'git'
+#versioneer.versionfile_source = 'timestream/_version.py'
+#versioneer.versionfile_build = 'timestream/_version.py'
+#versioneer.tag_prefix = ''
+#versioneer.parentdir_prefix = 'timestreamlib-'
+
+desc = """
+pyts2: Next-generation utilities and a python library for manipulating
+timelapses in the TimeStream format
+"""
+
+with open("requirements.txt") as fh:
+    install_requires = [req.strip() for req in fh]
+
+test_requires = [
+    "pytest",
+]
+
+
+setup(
+    name="pyts2",
+    packages=find_packages(),
+    #version=versioneer.get_version(),
+    #cmdclass=versioneer.get_cmdclass(),
+    install_requires=install_requires,
+    tests_require=test_requires,
+    #scripts=scripts,
+    description=desc,
+    author="Kevin Murray",
+    author_email="spam@kdmurray.id.au",
+    url="https://github.com/borevitzlab/timestreamlib",
+    keywords=["timestream", "timelapse", "photography", "video"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        ],
+    test_suite="test",
+    )
