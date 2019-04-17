4d4dbdfa0436b72d21d5ec153d8c072192925fe8
Kevin Murray <kdmfoss@gmail.com>
2019-04-17 18:02:26 +1000

    Better image io tests

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -3,6 +3,8 @@ from pyts2 import *
 from .data import *
 
 import numpy as np
+from PIL import Image
+from io import BytesIO
 
 class PretendTimestream(object):
     def __init__(self):
@@ -12,21 +14,27 @@ class PretendTimestream(object):
         self.files.append(file)
 
 
+def test_pipeline():
+    pass
 
 
 def test_encodedecodestep():
     def encode_decode_roundtrip(format):
-        print(format)
         pixels = np.array([[[255,255,255], [0, 0, 0]]], dtype="u1")
         instant = TSInstant.now()
-        filename = "pretend.file"
-        orig_image = TimestreamImage(instant=instant, pixels=pixels, filename=filename)
+        orig_image = TimestreamImage(instant=instant, pixels=pixels,
+                                     filename="pretend.file")
 
         encoded_image = EncodeImageFileStep(format=format).process_file(orig_image)
-
         assert isinstance(encoded_image, TimestreamFile)
         assert encoded_image.filename == f"pretend.{format}"
         assert encoded_image.instant == instant
+
+        # More involved checks: parse out the encoded bytes using pil, check format etc.
+        pil = Image.open(BytesIO(encoded_image.content))
+        expect_fmt = {"tif": "TIFF", "jpg": "JPEG", "png": "PNG"}
+        assert pil.format.upper() == expect_fmt[format]
+        assert pil.mode == "RGB"
 
         decoded_image = DecodeImageFileStep().process_file(encoded_image)
         assert isinstance(decoded_image, TimestreamImage)
